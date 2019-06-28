6c7f50c93ff7ccfbd092b8cb7dcda98652590b97
Luna <git@l4.pm>
2019-06-28 14:53:04 -0300

    add state serialization/deserialization

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,34 @@
 const std = @import("std");
+const states = @import("state.zig");
+const State = states.State;
+
+const OutError = std.fs.File.ReadError;
+const InError = std.fs.File.WriteError;
+
+fn do_search(state: *State, search_term: []u8) !void {
+    var state_file = try std.fs.File.openRead("state.bin");
+    var in = state_file.inStream();
+    var stream = &in.stream;
+    var deserial = std.io.Deserializer(.Big, .Bit, OutError).init(stream);
+
+    try deserial.deserializeInto(state);
+}
+
+fn do_build(state: *State, zig_std_path: []u8) !void {
+    var state_file = try std.fs.File.openWrite("state.bin");
+    var out = state_file.outStream();
+    var stream = &out.stream;
+    var serial = std.io.Serializer(.Big, .Bit, InError).init(stream);
+
+    try state.serialize(&serial);
+}
 
 pub fn main() anyerror!void {
     var arena = std.heap.ArenaAllocator.init(std.heap.direct_allocator);
     defer arena.deinit();
 
     var allocator = &arena.allocator;
+    var state = State.init(allocator);
 
     var args_it = std.process.args();
     if (!args_it.skip()) @panic("expected self arg");
@@ -13,8 +37,10 @@ pub fn main() anyerror!void {
 
     if (std.mem.eql(u8, action, "build")) {
         const zig_std_path = try (args_it.next(allocator) orelse @panic("expected zig stdlib path arg"));
+        try do_build(&state, zig_std_path);
     } else if (std.mem.eql(u8, action, "search")) {
         const search_term = try (args_it.next(allocator) orelse @panic("expected search term arg"));
+        try do_search(&state, search_term);
     } else {
         @panic("invalid action");
     }
--- src/state.zig
+++ src/state.zig
@@ -1,13 +1,73 @@
 const std = @import("std");
 
+pub const StateMap = std.AutoHashMap([]u8, []u8);
+
+fn serializeStr(serializer: var, string: []const u8) !void {
+    try serializer.serialize(@intCast(u29, string.len));
+    for (string) |byte| {
+        try serializer.serialize(byte);
+    }
+}
+
 pub const State = struct {
     allocator: *std.mem.Allocator,
+    map: StateMap,
 
     pub fn init(allocator: *std.mem.Allocator) State {
-        return State{ .allocator = allocator };
+        return State{
+            .allocator = allocator,
+            .map = StateMap.init(allocator),
+        };
     }
 
-    pub fn deserialize(self: *State, deserializer: var) !void {}
+    fn deserializeStr(
+        self: *State,
+        deserializer: var,
+        length_opt: ?u29,
+    ) ![]u8 {
+        var i: usize = 0;
+        var res: []u8 = undefined;
+        var length: u29 = undefined;
 
-    pub fn serialize(self: *State, serialize: var) !void {}
+        if (length_opt) |length_actual| {
+            length = length_actual;
+            res = try self.allocator.alloc(u8, length);
+        } else {
+            length = try deserializer.deserialize(u29);
+            res = try self.allocator.alloc(u8, length);
+        }
+
+        while (i < length) : (i += 1) {
+            var byte = try deserializer.deserialize(u8);
+            res[i] = byte;
+        }
+
+        return res;
+    }
+
+    pub fn deserialize(self: *State, deserializer: var) !void {
+        while (true) {
+            var length = try deserializer.deserialize(u29);
+            if (length == 0) break;
+
+            // deserialize a KV pair and put() it
+            const key = try self.deserializeStr(deserializer, length);
+            var value = try self.deserializeStr(deserializer, null);
+
+            _ = try self.map.put(key, value);
+        }
+    }
+
+    pub fn serialize(self: *State, serializer: var) !void {
+        var it = self.map.iterator();
+
+        while (it.next()) |kv| {
+            try serializeStr(serializer, kv.key);
+            try serializeStr(serializer, kv.value);
+        }
+
+        // sentry value to determine end of hashmap
+        // maybe we could just catch an endofstream instead, idk.
+        try serializer.serialize(u29(0));
+    }
 };
