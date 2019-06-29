2a0e0db025c3a47a35444e1060bffbf40efeb454
Luna <git@l4.pm>
2019-06-29 12:06:12 -0300

    remove State.addFile

--- src/state.zig
+++ src/state.zig
@@ -63,6 +63,7 @@ pub const State = struct {
 
     pub fn serialize(self: *State, serializer: var) !void {
         var it = self.map.iterator();
+        var cnt: usize = 0;
 
         while (it.next()) |kv| {
             std.debug.warn(
@@ -73,13 +74,18 @@ pub const State = struct {
 
             try serializeStr(serializer, kv.key);
             try serializeStr(serializer, kv.value);
+            cnt += 1;
         }
 
         // sentry value to determine end of hashmap
         // maybe we could just catch an endofstream instead, idk.
         try serializer.serialize(u29(0));
+
+        std.debug.warn("finished {} elements\n", cnt);
     }
 
+    /// Gets a given Node.DocComment and converts it to [][]const u8, with the
+    /// doc comment token trimmed out.
     fn docToSlice(self: *State, tree: var, doc_opt: ?*Node.DocComment) ![][]const u8 {
         if (doc_opt) |doc| {
             var it = doc.lines.iterator(0);
@@ -88,6 +94,7 @@ pub const State = struct {
             while (it.next()) |line_idx| {
                 lines = try self.allocator.realloc(lines, lines.len + 1);
                 var line = tree.tokenSlice(line_idx.*);
+
                 lines[lines.len - 1] = std.mem.trimLeft(u8, line, "/// ");
             }
 
@@ -97,73 +104,7 @@ pub const State = struct {
         }
     }
 
-    /// Add a file from the zig standard library into the state.
-    pub fn addFile(self: *State, rel_path: []const u8, full_path: []const u8) !void {
-        std.debug.warn("adding file: '{}'..", rel_path);
-        var file = try std.fs.File.openRead(full_path);
-        defer file.close();
-
-        const total_bytes = try file.getEndPos();
-        std.debug.warn("({} bytes)..", total_bytes);
-
-        if (total_bytes > (800 * 1024)) {
-            std.debug.warn("SKIP (too much)\n");
-            return;
-        }
-
-        var data = try self.allocator.alloc(u8, total_bytes);
-        const bytes_read = try file.read(data);
-
-        // safety of life check
-        std.testing.expectEqual(bytes_read, total_bytes);
-
-        var tree = try std.zig.parse(self.allocator, data);
-        var root = tree.root_node;
-        defer tree.deinit();
-
-        var idx: usize = 0;
-
-        // fun fact, the easier case is handling a raw FnProto, this wasn't
-        // obvious at first.
-        while (root.iterate(idx)) |child| : (idx += 1) {
-            switch (child.id) {
-                .VarDecl => blk: {
-                    var decl = @fieldParentPtr(Node.VarDecl, "base", child);
-
-                    var visib_tok_opt = decl.visib_token;
-                    if (visib_tok_opt) |visib_tok| {
-                        std.debug.warn(
-                            "pub var, name='{}'\n",
-                            tree.tokenSlice(decl.name_token),
-                        );
-
-                        var lines = try self.docToSlice(tree, decl.doc_comments);
-                        for (lines) |line| {
-                            std.debug.warn("\tdoc: '{}'\n", line);
-                        }
-
-                        // check if from there the rhs is a struct or not,
-                        // and if it is, recursively go through its members
-                        // and add them to the state.map.
-                        var suffix = @fieldParentPtr(Node.SuffixOp, "base", decl.init_node.?);
-                        switch (suffix.op) {
-                            .StructInitializer => |struct_list| blk: {
-                                std.debug.warn("!VAR IS STRUCT\n");
-                            },
-                            else => std.debug.warn("var not struct\n"),
-                        }
-                    }
-                },
-
-                else => continue,
-            }
-        }
-
-        std.debug.warn("OK\n");
-
-        _ = try self.map.put(rel_path, data);
-    }
-
+    /// Add a given node (not all by itself) into the state.
     pub fn addNode(
         self: *State,
         tree: *Tree,
